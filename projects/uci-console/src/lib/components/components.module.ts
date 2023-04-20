732c162893272ca6bb1ec986dce289c6876cb8bd
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-04-20 13:31:26 +0530

    ED-1275 fix:Angular-13 changes

--- projects/uci-console/src/lib/components/components.module.ts
+++ projects/uci-console/src/lib/components/components.module.ts
@@ -11,7 +11,7 @@ import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {ConversationListComponent} from './conversation-list/conversation-list.component';
 import {ConversationAddComponent} from './conversation-add/conversation-add.component';
 import {UserSegmentListComponent} from './user-segment-list/user-segment-list.component';
-import {CommonFormElementsModule} from '@project-sunbird/common-form-elements';
+import {CommonFormElementsModule} from '@project-sunbird/common-form-elements-full';
 import {UserSegmentAddComponent} from './user-segment-add/user-segment-add.component';
 import {SuiModule} from 'ng2-semantic-ui-v9';
 import {ConversationSuccessComponent} from './conversation-success/conversation-success.component';
